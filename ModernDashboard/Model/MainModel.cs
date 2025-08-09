39ec1529d7a86997922ad858b04b179459bf46b2
Neha <nehach2513@gmail.com>
2025-08-10 01:20:40 +0530

    Initial commit

---
+++ ModernDashboard/Model/MainModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+//<summary>
+//Model ["The content creator"]
+//The model class holds the data. The model can be referred to as the file for the front-end of the application.
+//<summary>
+
+namespace ModernDashboard.Model
+{
+    //Main menu items
+    public class MenuItems
+    {
+        public string? MenuName { get; set; }
+        public string? MenuImage { get; set; }
+    }
+    //Home Page 
+    public class HomeItems
+    {
+        public string? HomeName { get; set; }
+        public string? HomeImage { get; set; }
+    }
+    //PC Page 
+    public class PCItems
+    {
+        public string? PCName { get; set; }
+        public string? PCImage { get; set; }
+    }
+    //Desktop Page
+    public class DesktopItems
+    {
+        public string? DesktopName { get; set; }
+        public string? DesktopImage { get; set; }
+    }
+    //Document Page 
+    public class DocumentItems
+    {
+        public string? DocumentName { get; set; }
+        public string? DocumentImage { get; set; }
+    }
+    //Download Page
+    public class DownloadItems
+    {
+        public string? DownloadName { get; set; }
+        public string? DownloadImage { get; set; }
+    }
+    //Picture Page
+    public class PictureItems
+    {
+        public string? PictureName { get; set; }
+        public string? PictureImage { get; set; }
+    }
+    //Music Page 
+    public class MusicItems
+    {
+        public string? MusicName { get; set; }
+        public string? MusicImage { get; set; }
+    }
+    //Movie Page
+    public class MovieItems
+    {
+        public string? MovieName { get; set; }
+        public string? MovieImage { get; set; }
+    }
+    //Trash Page
+    public class TrashItems
+    {
+        public string? TrashName { get; set; }
+        public string? TrashImage { get; set; }
+    }
+}
---
+++ ModernDashboard/RelayCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Windows.Input;
+
+namespace ModernDashboard
+{
+    //Summary
+    //RelayCommand allows you to inject the ccommand's logic via delegates passed into its constructor.
+    //this method enables ViewModel classes to impplement command
+    //Summary
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        public RelayCommand(Action<object> execute)
+        {
+            this.execute = execute;
+            canExecute = null;
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        // <Summary>
+        // canExecuteChanged delegates the event subscription to the CommandManager. RequerySuggested event.
+        // This ensures that the WPF commanding infrastructure asks all RelayCommand objects if they can execute whenever 
+        // It asks the built-in commands
+        // <Summary>  
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || CanExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            execute(parameter)
+        }
+
+
+        //public event EventHandler? CanExecuteChanged;
+
+        //public bool CanExecute(object? parameter)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Execute(object? parameter)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+}
